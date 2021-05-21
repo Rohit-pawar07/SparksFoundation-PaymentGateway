8706753e10baac97d8ea1e31c57a215cb3f5bc3f
Rohit-pawar07 <rohithpawar007@gmail.com>
2021-05-21 22:37:20 +0530

    Project Added

---
+++ Charitypage/donate.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Donate</title>
+</head>
+<body>
+<img class="bg"src="charity.jpg"alt="...">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<a class="navbar-brand" href="#"></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="navbar-nav">
+  <a type="button" href="index.php" class="btn btn-success" style="margin-left : 8px; background-color:	#FCFFF0		; color:#D02090	;font-weight: bold; ">Home</a>
+  <a type="button" href="donate.php" class="btn btn-danger" style="margin-left : 8px; font-weight: bold;background-color: #DC143C ">Donate</a>
+  <a type="button" href="https://www.thesparksfoundationsingapore.org/" class="btn btn-info" style="margin-left : 8px ;background-color: #FCFFF0;color:#D02090; font-weight: bold;" target="_blank;">About us</a>
+  </div>
+</div>
+</nav>x
+
+  <div id="content" class="container" >
+    <div class="row">
+      <div class="col"></div>
+      <div class="col-8">
+        <br>
+
+        <h1 class="cover-heading" style="font-family:Bernard MT Condensed;font-size: 60px; color:black" > Donate</h1>
+        <img src="heart.png" style="height: 60px;width: 70px;margin-left:25%;">
+        <p class="lead"style="font-family:Gadugi">
+        </p>
+        <p class="lead">
+        <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HDVHJ1LF84v4Fh" async> </script> </form>
+      </div>
+      <div class="col"></div>
+    </div>
+  </div>
+
+  <footer class="footer" style="background-color:#4d4d4d">
+  <div class="footer-widget-section">
+  <div class="container text-center">
+      <div class="row">
+          <div class="col-sm-4 footer-block">
+              <div class="footer-widget widget_text">
+                  <div class="footer-logo">
+                  <br><br><a href="#"><img style="height: 70px;width:70px"src="developer.jpg" alt=""></a>
+                  </div>
+                  <p style="color:#fff;"><br>Made with ❤️ by Rohit Pawar</p>
+                  <p style="color:#fff;"><a href="#"></a>Under A Web Development Internship</p>
+                  <p style="color:#fff;"><a href="#"></a>Sparks Foundation</p>
+                  <p style="color:#fff;"><a href="#"></a>#GRIPMAY2021</p>
+              </div>
+          </div>
+          <div class="col-sm-4 footer-block">
+              <div class="footer-widget widget_text">
+              <br><br><h3   style="color:#fff;">About Us</h3>
+                  <p style="color:#fff;">Website for Donating Money to Charity Child NGO</p><br>
+              </div>
+          </div>
+          <div class="col-sm-4 footer-block last">
+              <div class="footer-widget widget_text">
+                  <br><br><h3 style="color:#fff;">Contact Us</h3>
+                  <address style="color:#fff;">
+                  Call Us 123456789<br>
+                  Send an Email on <a href="mailto:#">charitychild@ngo.com</a><br>
+                  Visit Us New Delhi<br>India
+                  </address>
+          </div>
+      </div>
+  </div>
+  </div>
+  <script src="js/jquery-2.1.4.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="js/smoothscroll.js"></script>
+  <script src="js/mobile-menu.js"></script>
+  <script src="js/flexSlider/jquery.flexslider-min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js"></script>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
+<style>
+.bg{
+    width:100%;
+    height: 100%;
+    position:absolute;
+    z-index:-1;
+    opacity: 0.8;
+    background-attachment: fixed;
+}
+#content {
+
+      margin-top:12%;
+      justify-content:right;
+        width: 330px;
+      height: 350px;
+
+      color: white;
+    }
+.footer-widget{
+    color: #dcdcdc;
+    font-size: 13.5px;
+}
+.footer {
+  .footer-widget{
+    color: #dcdcdc;
+    font-size: 18px;
+}
+</style>
+
+
+<!-- <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HDVHJ1LF84v4Fh" async> </script> </form> -->
---
+++ Charitypage/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Charity Child❤️</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark" style="color: #ADADAD;">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+            <a type="button" href="home.php" class="btn btn" style="margin-right: 5px; background-color:	#FCFFF0		; color:#D02090	;font-weight: bold;" >Home</a>
+            <a type="button" href="https://www.thesparksfoundationsingapore.org/" class="btn btn-info" style= "margin-right : 8px;background-color: #FCFFF0	;color:#D02090; font-weight: bold;" target="_blank">About Us</a>
+            <a type="button" href="donate.php" class="btn btn-danger" style= "margin-right:  5px;font-weight: bold;background-color: #DC143C	;">Donate</a>
+      </div>
+    </div>
+    <div class="top" style="color:#FCFFF0	;" >
+    <h2 style="text-align: center;font-size:40px;">❤️Charity Child❤️</h2>
+    </div>
+    </nav>
+  <div class="container" style="padding-left: 0px; padding-right: 0px;max-width: 100%;" >
+    <img style=" display: block; background-position: center;
+    background-repeat: no-repeat; background-size: cover; width: 100%;" src="child2.png" id="image">
+  </div>
+  <div class="container1" style="position: absolute; bottom: 8px; right: 16px; color:#FFFFFF;position: absolute;
+  bottom: 20px;right: 20px;background-color: black;color: white;padding-left: 20px;padding-right: 20px;">
+
+  <p>Chariter is the number one Nonprofit organization website template solution to spread your causes with style.
+     This modern, easy to use and all-around site canvas equips you with many practical amenities to start working on your charity website right away.
+     It is an HTML template, So coding knowledge will be necessary to activate Chariter. Still, the code structure is organized, perfect for beginners and experts.</p>
+  </div>
+
+  <footer class="footer" style="background-color:#4d4d4d">
+    <h1 style="text-align:center; color: #FCFFF0;">Support a Cause , You care About !</h1>
+    <div class="footer-widget-section">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-sm-4 footer-block">
+                    <div class="footer-widget widget_text">
+                      <div class="footer-logo">
+                        <br><br><a href="#"><img style="height: 100px; width:100px; text-align: justify;"src="developer.jpg" alt=""></a>
+                        </div>
+                        <p style="color:#fff;"><br>Made with ❤️ by Rohit Pawar</p>
+                        <p style="color:#fff;"><a href="#"></a>Under A Web Development Internship</p>
+                        <p style="color:#fff;"><a href="#"></a>Sparks Foundation</p>
+                        <p style="color:#fff;"><a href="#"></a>#GRIPMAY2021</p>
+                    </div>
+                </div>
+                <div class="col-sm-4 footer-block">
+                    <div class="footer-widget widget_text" style="text-align: center;">
+                    <br><br><h3 style="color:#fff;">About Us</h3>
+                        <p style="color:#fff;">Website for Donating Money to Charity Child NGO</p><br>
+                    </div>
+                </div>
+                <div class="col-sm-4 footer-block last">
+                    <div class="footer-widget widget_text">
+                        <br><br><h3 style="color:#fff;">Contact Us</h3>
+                        <address style="color:#fff;">
+                            Call Us 123456789<br>
+                            Send an Email on <a href="mailto:#">charitychild@ngo.com</a><br>
+                            Visit Us New Delhi<br>India
+                        </address>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/mobile-menu.js"></script>
+    <script src="js/flexSlider/jquery.flexslider-min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
